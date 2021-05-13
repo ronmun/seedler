df4a405a1bc219839bcef8906d187b154105e87d
Carlos Guerrero <fulguris21@gmail.com>
2021-05-13 13:40:51 -0500

    Added bridge and button

---
+++ Assets/Scripts/Props/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject platform;
+    private bool isPlatformActive = false;
+    public GameObject leaf;
+    private Renderer leafRenderer;
+    private Color originalLeafColor;
+    void Start()
+    {
+        leafRenderer = leaf.GetComponent<Renderer>();
+        originalLeafColor = leafRenderer.material.GetColor("_Color");
+        platform.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider obj) {
+        if(obj.gameObject.tag == "Player") {
+            isPlatformActive = !isPlatformActive;
+            platform.SetActive(isPlatformActive);
+
+            if(isPlatformActive) {
+                leafRenderer.material.SetColor("_Color", Color.red);
+            } else {
+                leafRenderer.material.SetColor("_Color", originalLeafColor);
+            }
+            
+        }
+     }
+
+}
