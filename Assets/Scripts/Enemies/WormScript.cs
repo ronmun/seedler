33b6bc9abd4470602d9dccc1f61b18375dbbdf49
Aarón Gómez de Segura G <aaron.gomez.de.segura@gmail.com>
2021-05-16 23:02:58 -0500

    Added level 3 and Worm enemies animation

--- Assets/Scripts/Enemies/WormScript.cs
+++ Assets/Scripts/Enemies/WormScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class WormScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Transform> points;
+
+    private float speed = 2.0f;
+    private float distance = 0.2f;
+
+    private int nextPoint = 0;
+    private float platformReady = 0;
+    private float platformCooldown = 4f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = Vector3.MoveTowards(this.transform.position,
+            points[nextPoint].transform.position,
+            speed * Time.deltaTime);
+
+        if (Vector3.Distance(this.transform.position, points[nextPoint].transform.position) < this.distance)
+        {
+            if (Time.time > platformReady)
+            {
+                platformReady = Time.time + platformCooldown;
+                this.nextPoint++;
+                if (this.nextPoint >= points.Count)
+                {
+                    this.nextPoint = 0;
+                }
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            // Hacer daño al jugador
+        }
     }
 }
