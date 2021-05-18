ba9f31d3e995fc0905dd04b20f7a58350d000b94
Aarón Gómez de Segura G <aaron.gomez.de.segura@gmail.com>
2021-05-18 16:00:36 -0500

    Ultimate Change

---
+++ Assets/Scripts/Camera/TransitionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScript : MonoBehaviour
+{
+    public float bonusTime;
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(bonusTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
