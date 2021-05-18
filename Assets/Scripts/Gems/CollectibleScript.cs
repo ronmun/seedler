99eb72652d8577402a899617cede20a57fa9c226
Aarón Gómez de Segura G <aaron.gomez.de.segura@gmail.com>
2021-05-18 01:16:08 -0500

    Basic Audio SFX

---
+++ Assets/Scripts/Gems/CollectibleScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public void PlaySound()
+    {
+        audio.Play();
+    }
+}
